c6f2630605d784b20518b85e1cdc5dd5e209b904
Norika Narimatsu <nnarimat@f1r7s3.codam.nl>
2025-08-29 15:43:20 +0200

    uodate

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,6 @@
 const el = document.querySelector<HTMLDivElement>("#app")!;
 el.innerHTML = `
   <main style="font-family: system-ui; padding: 1rem">
-    <h1>Frontend (no Vite) is alive !!!</h1>
-    <p>TypeScript compiled to plain JS.</p>
+    <h1>Frontend is alive !!!</h1>
   </main>
 `;
