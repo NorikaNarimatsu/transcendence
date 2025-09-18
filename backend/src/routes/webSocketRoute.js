e091fde857b14b20a5d8a9dcd22580a3e146eeea
edooarda <dudamakesthings@gmail.com>
2025-09-18 10:54:26 +0200

    fix websocket return

--- backend/src/routes/webSocketRoute.js
+++ backend/src/routes/webSocketRoute.js
@@ -2,14 +2,15 @@
 
 export default async function (app) {
   app.get('/game', { websocket: true }, (connection, request) => {
-    connection.socket.on('message', message => {
+    console.log('Websocket client connected');
+    connection.on('message', message => {
       // Example: handle player move and send result
     //   const result = handlePlayerMove('player1', message.toString());
     //   connection.socket.send(JSON.stringify(result));
-        connection.socket.send('pong: ' + message);
+        connection.send('pong: ' + message);
     });
 
-    connection.socket.on('close', () => {
+    connection.on('close', () => {
       // Handle disconnect if needed
       console.log('Client disconnected');
     });
