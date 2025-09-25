1ef1ced15b200c2491c20a83b70e7710ffd602f4
Sara Ramos <sara@Saras-MacBook-Air.local>
2025-09-25 17:59:28 +0200

    Started Player Profile Page

--- frontend/src/pages/PlayerProfile.tsx
+++ frontend/src/pages/PlayerProfile.tsx
@@ -1,4 +1,5 @@
 import type { JSX } from 'react';
+import avatar1 from '../assets/avatars/Avatar 1.png'
 
 export default function PlayerProfile(): JSX.Element{
     return (
@@ -8,12 +9,33 @@ export default function PlayerProfile(): JSX.Element{
             </header>
             <section className="flex-1 bg-pink-grid flex items-center justify-center">
                 <div className="relative bg-pink-dark w-[1000px] h-[600px] m-[10px] flex justify-between items-center px-[50px]">
-                    <div className="relative bg-pink-light w-[300px] h-[500px]"></div>
+                    {/* Player Card */}
+                    <div className="shadow-no-blur bg-pink-light w-[350px] h-[500px] flex flex-col justify-between py-6">
+                        {/* PLAYER INFO title */}
+                        <div className="font-pixelify text-white text-5xl text-center text-shadow-xs">PLAYER INFO</div>
+                        
+                        {/* Avatar and player info */}
+                        <div className="bg-pink-dark mx-[25px] h-[125px] border-purple flex flex-row justify-center items-center px-4">
+                            <img src={avatar1} alt="Avatar 1" className="avatar m-auto" />
+                            <div className="flex flex-col m-auto">
+                                <div className="font-pixelify text-white text-5xl">Norika</div>
+                                <div className="font-pixelify text-white text-3xl">700 XP</div>
+                            </div>
+                        </div>
+                        
+                        {/* Buttons container - evenly distributed */}
+                        <div className="flex flex-col justify-between flex-grow mx-[25px] py-4">
+                            <button className="button-pp">Games</button>
+                            <button className="button-pp">Friends</button>
+                            <button className="button-pp">Scores</button>
+                            <button className="button-pp-settings">Settings</button>
+                        </div>
+                    </div>
+                    {/* Right side content */}
                     <div className="relative bg-blue-deep w-[500px] h-[500px]"></div>
                 </div>
             </section>
             <footer className="h-40 bg-blue-deep"></footer>
-
         </main>
     )
 }
